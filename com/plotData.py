2476c3af41492c58f0139ec7bc08e208a3c6f708
Marco Willgren <mhw1@hotmail.com>
2014-11-11 11:07:43 +0200

    Solution for first task

--- com/plotData.py
+++ com/plotData.py
@@ -10,10 +10,27 @@ basepath = os.path.dirname(__file__)
 filepath = os.path.abspath(os.path.join(basepath, "..", "wine.data"))
 
 x = np.loadtxt(filepath, delimiter=',', usecols=[0])
-y = np.loadtxt(filepath, delimiter=',', skiprows = 0)[1:]
+y = np.loadtxt(filepath, delimiter=',', usecols=range(1, 14))
 
-print x
-print y
+features = ['Alcohol', 'Malic acid', 'Ash', 'Alcality of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols',
+            'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+
+
+for i in range(0, 12):
+    hist = []
+    for j in range (0, len(y) - 1):
+        hist.append(y[j,i])
+    pp.grid(True)
+    pp.title('Feature ' + `i + 1` + ': ' + features[i])
+    pp.ylabel('Count')
+    pp.xlabel('Value')
+    pp.hist(hist)
+    pp.show()
+        
+
+
+'''print x
+print y'''
 
 
 '''pp.title('TITLE')
