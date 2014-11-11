d7adbf55d99f6e7381a87bb91b97e8667a10005d
Marco Willgren <mhw1@hotmail.com>
2014-11-11 20:52:16 +0200

    Solutions for tasks 5 and 6

--- com/plotData.py
+++ com/plotData.py
@@ -1,35 +1,40 @@
 import numpy as np
 from matplotlib import pyplot as pp
 import os
-from numpy.f2py.auxfuncs import throw_error
 
 
 if __name__ == '__main__':
     pass
 
+
+# Set pointer to correct destination
 basepath = os.path.dirname(__file__)
 filepath = os.path.abspath(os.path.join(basepath, "..", "wine.data"))
 
+# Parse 'wine.data' file into two variables x, y -> (x = labels, y = feature data).
 x = np.loadtxt(filepath, delimiter=',', usecols=[0])
 y = np.loadtxt(filepath, delimiter=',', usecols=range(1, 14))
 
-features = ['Alcohol', 'Malic acid', 'Ash', 'Alcality of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols',
+# Label each feature for sake of clarity. Used in creating plot views.
+featureLabels = ['Alcohol', 'Malic acid', 'Ash', 'Alcality of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols',
             'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
 
-        
+
+# Task 1: Plot a histogram of each feature       
 def defaultHistogram(matrix):
     for i in range(0, 13):
         hist = []
         for j in range (0, len(y)):
             hist.append(matrix[j,i])
         pp.grid(True)
-        pp.title('Feature ' + `i + 1` + ': ' + features[i])
+        pp.title('Feature ' + `i + 1` + ': ' + featureLabels[i])
         pp.ylabel('Count')
         pp.xlabel('Value')
         pp.hist(hist, 30, facecolor='#800020', normed=1, alpha=0.8)
         pp.show()
 
 
+# Task 2: Plot the same histograms and visualize the classes. 
 def visualizeByClasses(array, matrix):
     for i in range(0, 13):
         hist1 = []
@@ -43,9 +48,9 @@ def visualizeByClasses(array, matrix):
             elif array[j] == 3:
                 hist3.append(matrix[j,i])
             else:
-                throw_error
+                return
         pp.grid(True)
-        pp.title('Feature ' + `i + 1` + ': ' + features[i])
+        pp.title('Feature ' + `i + 1` + ': ' + featureLabels[i])
         pp.ylabel('Count')
         pp.xlabel('Value')
         pp.hist([hist1, hist2, hist3], 15, color=['#800020', '#F7E7CE', '#673147'], label=['Class1', 'Class2', 'Class3'],
@@ -53,8 +58,10 @@ def visualizeByClasses(array, matrix):
         pp.legend()
         pp.show()
         
-# One of the features: 'Proline', has significantly greater values than the other features. Therefore it dominates
-# the plot        
+
+# Task 3: Make a parellel plot of the features. 
+# One of the features: 'Proline', has/maps to significantly larger values than the other features. 
+# Therefore it dominates the plot over other features       
 def parallelFeatures(matrix):
     for i in range(0, 13):
         plot = []
@@ -67,29 +74,78 @@ def parallelFeatures(matrix):
     pp.title('Parallel plot of features')
     pp.show()
     
-    
-def calculateCorrelations(matrix):
-    plotMatrix = []
-    for i in range(0, 13):
-        plot = []
-        for j in range(0, len(y)):
-            plot.append(matrix[j][i])
-        plotMatrix.append(plot)
 
-    for k in range(0, len(plotMatrix)):
-        for z in range(k + 1, len(plotMatrix)):
-            array = np.corrcoef(plotMatrix[k], plotMatrix[z])
-            for r in range(0, len(array)):
-                if 0.7 <= array[r,1] <= 1 or -0.7 >= array[r,1] <= -1:
-                    print 'FeatureA: '
-                    print k
-                    print 'FeatureB: '
-                    print z
-                    print 'correlation factor: ' 
-                    print array[r,1]
+# Task 4: Calculate the correlation coefficients.
+# The correlation coeffecients are saved to the 'correlations' variable (matrix).
+# The strongly correlating features (between [0,7..1 or -0,7..-1]) are printed also    
+def calculateCorrelations(matrix):
+    featureMatrix = []
+    interestingCorrelations = []
+    for i in range(0, 13):
+        features = []
+        for j in range(0, len(y)):
+            features.append(matrix[j][i])
+        featureMatrix.append(features)
+
+    for k in range(0, len(featureMatrix)):
+        for z in range(k + 1, len(featureMatrix)):
+            correlations = np.corrcoef(featureMatrix[k], featureMatrix[z])
+            print correlations 
+            for r in range(0, len(correlations)):
+                if 0.7 <= correlations[r,1] <= 1 or -0.7 >= correlations[r,1] <= -1:
+                    interestingCorrelations.append(k)
+                    interestingCorrelations.append(z)
+                    print
+                    print 'FeatureA: ' + str(featureLabels[k]) + '(' + str(k) + ')'
+                    print 'FeatureB: ' + str(featureLabels[z]) + '(' + str(z) + ')'
+                    print 'Correlation factor: ' + str(correlations[r,1])
+                    print 
                 break
-                    
-#defaultHistogram(y)
-#visualizeByClasses(x, y)
-#parallelFeatures(y)
-calculateCorrelations(y)
+    return interestingCorrelations
+
+
+# Task 5: Visualize the interesting correlations with the scatter plot
+def visualizeAsScatterPlot(matrix):
+    strongCorrelations = calculateCorrelations(y)
+    
+    i = 0
+    while i < len(strongCorrelations):
+        first = strongCorrelations[i]
+        second = strongCorrelations[i + 1]
+        i = i + 2
+        firstFeatureData = matrix[:,first]
+        secondFeatureData = matrix[:, second]
+        
+        pp.title('Scatter plot of features: ' + featureLabels[first] + ' & ' + featureLabels[second])
+        pp.scatter(firstFeatureData, secondFeatureData, s=60, c=['red', 'blue'], label='Scatter', alpha=0.5)
+        pp.legend()
+        pp.show()    
+
+
+# Task 6: Calculate Covariance matrix -> calculate eigenvalues and eigenvectors from square covariance matrix
+def calculateConvarianceAndEigen(matrix):
+    tempMatrix = []
+    for i in range(0, 13):
+        featureData = []
+        for j in range (0, len(y)):
+            featureData.append(matrix[j,i])
+            tempMatrix.append(featureData)
+        # featureCovariance = np.cov(featureData) # From each feature -- 1-D array
+        # covarianceMatrix =  np.cov(tempMatrix) # From each feature matrix -- 2-D array
+    bigCovarianceMatrix = np.cov(tempMatrix) # From the whole data set matrix -- 2-D array
+    eigVal, eigVec = np.linalg.eig(bigCovarianceMatrix)
+    print
+    print 'Eigenvalues: ' + str(eigVal)
+    print
+    print 'Eigenvectors: ' + str(eigVec)
+    
+
+# The primary method calls are located here.                    
+# defaultHistogram(y) # Task 1
+# visualizeByClasses(x, y) # Task 2
+# parallelFeatures(y) # Task 3
+# calculateCorrelations(y) # Task 4
+# visualizeAsScatterPlot(y) # Task 5
+# calculateConvarianceAndEigen(y) # Task 6
+
+
