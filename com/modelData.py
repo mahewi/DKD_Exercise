06a685951b715740c598a9a7d297bb614f79448d
Marco Willgren <mhw1@hotmail.com>
2014-12-09 22:35:05 +0200

    Deleted unused imports, created methods for clarity and added better printing

--- com/modelData.py
+++ com/modelData.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
 import os
 from matplotlib import pyplot as pp
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 from patsy.highlevel import dmatrices
-from sklearn.cross_validation import StratifiedKFold as SKF
 from sklearn.cross_validation import StratifiedShuffleSplit as SSS
 
 if __name__ == '__main__':
@@ -24,69 +22,79 @@ y = np.loadtxt(filepath, delimiter=',', usecols=range(0,1))
 labels = ['Class','Alcohol', 'Malic_acid', 'Ash', 'Alcality_of_ash', 'Magnesium', 'Total_phenols', 'Flavanoids', 'Nonflavanoid_phenols',
             'Proanthocyanins', 'Color_intensity', 'Hue', 'OD280_OD315_of_diluted_wines', 'Proline']
 
+
 def linearRegression(matrix):
     df = pd.DataFrame(matrix,columns=labels)
-    y, X = dmatrices('Class ~ Alcohol + Malic_acid + Ash + Alcality_of_ash'+
-                     '+ Magnesium + Total_phenols + Flavanoids + Nonflavanoid_phenols + Proanthocyanins + Color_intensity'+
-                     '+ Hue + OD280_OD315_of_diluted_wines + Proline', 
-                     df, return_type='matrix')
+    y, X = dmatrices(generateLabels(), df, return_type='matrix')
     y = np.ravel(y)
-    regModel = LogisticRegression()
-    regModel = regModel.fit(X,y);
-    predictedY = regModel.predict(X)
-    numOfErrors =  calculateNumOfErrors(y, predictedY)
-    print 'Number of errors in whole data: \n' + str(numOfErrors)
+    errorCount =  inferErrors(X, y)
+    print 'Errors in whole data: ' + str(errorCount)
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=np.random)
-    regModelTrain = LogisticRegression()
-    regModelTrain = regModelTrain.fit(X_train,y_train)
-    predictedYTrain = regModelTrain.predict(X_train)
-    numOfErrorsTrain = calculateNumOfErrors(y_train, predictedYTrain)
-    print 'Number of errors in training data 2: \n' + str(numOfErrorsTrain)
+    trainErrorCount = inferErrors(X_train, y_train)
+    print 'Errors in randomized training data (3/4): ' + str(trainErrorCount)
     
-    regModelTest = LogisticRegression()
-    regModelTest = regModelTest.fit(X_test,y_test)
-    predictedYTest = regModelTest.predict(X_test)
-    numOfErrorsTest = calculateNumOfErrors(y_test, predictedYTest)
-    print 'Number of errors in test data 2: \n' + str(numOfErrorsTest)
-    sss = SSS(y,10,test_size=0.25,random_state=np.random)
+    testErrorCount = inferErrors(X_test, y_test)
+    print 'Errors in randomized test data (1/4): ' + str(testErrorCount)
+    
+    stratifiedShuffleSplit = SSS(y,10,test_size=0.25,random_state=np.random)
     i = 0
     numOfTrainErrors = []
     numOfTestErrors = []
-    for train_index, test_index in sss:
+    for train_index, test_index in stratifiedShuffleSplit:
         i = i+1
-        #print('TRAIN:', train_index, 'TEST:', test_index)
         X_2train, X_2test = X[train_index],X[test_index]
         y_2train,y_2test = y[train_index],y[test_index]
-        regModelTrain = LogisticRegression()
-        regModelTrain = regModelTrain.fit(X_2train, y_2train)
-        predictedY = regModelTrain.predict(X_2train)
-        numOfErrorsTrain = calculateNumOfErrors(y_2train, predictedY)
-        numOfTrainErrors.append(numOfErrorsTrain)
-        print 'Number of errors in training data (10-fold) ' + str(i) + ':\n' + str(numOfErrorsTrain)
-        regModelTest = LogisticRegression()
-        regModelTest = regModelTest.fit(X_2test,y_2test)
-        predictedYTest = regModelTest.predict(X_2test)
-        numOfErrorsTest = calculateNumOfErrors(y_2test, predictedYTest)
-        numOfTestErrors.append(numOfErrorsTest)
-        print 'Number of errors in test data (10-fold) ' + str(i) + ':\n' + str(numOfErrorsTest)
+        numOfErrorsTrain = inferErrors(X_2train, y_2train)
+        numOfTrainErrors.append(numOfErrorsTrain)        
         
+        numOfErrorsTest = inferErrors(X_2test, y_2test)
+        numOfTestErrors.append(numOfErrorsTest)
+    
+    print
+    print 'Errors in training data sets (10-fold)'
+    for i in range(0, len(numOfTrainErrors)):
+        print 'Set ' + str(i + 1) + ': ' + str(numOfTrainErrors[i])
+    
+    print
+    print 'Errors in test data sets (10-fold)'
+    for i in range(0, len(numOfTestErrors)):
+        print 'Set ' + str(i + 1) + ': ' + str(numOfTestErrors[i])
+         
     numOfTrainErrors = np.array(numOfTrainErrors)
     numOfTestErrors = np.array(numOfTestErrors)
+    print
     print 'Std deviation of train data: ' + str(numOfTrainErrors.std())
     print 'Std deviation of test data: ' + str(numOfTestErrors.std())
-    print 'Mean of train data: ' + str(numOfTrainErrors.mean())    
+    print 'Mean of training data: ' + str(numOfTrainErrors.mean())    
     print 'Mean of test data: ' +str(numOfTestErrors.mean())
     
-    data = [numOfTrainErrors, numOfTestErrors]
+    createBoxPlot(numOfTrainErrors, numOfTestErrors)
+
+    
+def calculateErrorCount(clazz,predictedValues):
+    return len(filter(None,[predictedValuese if clazze != predictedValuese else '' for clazze, predictedValuese in zip(clazz,predictedValues)]))
+ 
+
+def inferErrors(xData, yData):
+    regressionModel = LogisticRegression()
+    regressionModel = regressionModel.fit(xData,yData);
+    predictedY = regressionModel.predict(xData)
+    return calculateErrorCount(yData, predictedY)
+
+
+def createBoxPlot(trainErrCount, testErrCount):
+    data = [trainErrCount, testErrCount]
     colors = ['cyan','pink']
     pp.figure()
-    box = pp.boxplot(data, labels=['Train','Test'], patch_artist=True)
+    box = pp.boxplot(data, labels=['Training','Test'], patch_artist=True)
     for patch, color in zip(box['boxes'], colors):
         patch.set_facecolor(color)
     pp.show()
-    
-def calculateNumOfErrors(clazz,predictedValues):
-    return len(filter(None,[predictedValuese if clazze != predictedValuese else '' for clazze, predictedValuese in zip(clazz,predictedValues)]))
+
+
+def generateLabels():
+    return str(labels[0]) + '~' + "+".join(labels[1:])
+
       
-linearRegression(x)
+linearRegression(x) # Task 1a)
